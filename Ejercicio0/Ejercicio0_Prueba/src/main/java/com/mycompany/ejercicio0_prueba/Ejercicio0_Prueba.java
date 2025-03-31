5f9825de405dd1e1a08076afc2563568e152fae0
daniel-przm <danipm89@gmail.com>
2025-03-31 13:12:26 +0200

    He añadido un comentario hola

--- Ejercicio0/Ejercicio0_Prueba/src/main/java/com/mycompany/ejercicio0_prueba/Ejercicio0_Prueba.java
+++ Ejercicio0/Ejercicio0_Prueba/src/main/java/com/mycompany/ejercicio0_prueba/Ejercicio0_Prueba.java
@@ -4,6 +4,6 @@ package com.mycompany.ejercicio0_prueba;
 public class Ejercicio0_Prueba {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //Hola
     }
 }
