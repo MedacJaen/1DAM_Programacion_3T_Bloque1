7aa696dbd252d395a9ff03f4db58ca6922db0c85
danielpmedac <daniel.perez@doc.medac.es>
2025-03-31 14:11:12 +0200

    Ejemplo excepciones

--- Ejercicio0/Ejercicio0_Prueba/src/main/java/com/mycompany/ejercicio0_prueba/Ejercicio0_Prueba.java
+++ Ejercicio0/Ejercicio0_Prueba/src/main/java/com/mycompany/ejercicio0_prueba/Ejercicio0_Prueba.java
@@ -1,9 +1,37 @@
 
 package com.mycompany.ejercicio0_prueba;
 
+import java.util.Scanner;
+
 public class Ejercicio0_Prueba {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); //Hola
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int[] numeros = {5,2,6};
+        
+        System.out.println("Que numero quieres ver?");   
+        
+        try {
+            int num1 = Integer.parseInt( sc.nextLine() );
+            System.out.println(numeros[num1]);
+        
+        } catch(ArrayIndexOutOfBoundsException aio) {
+            System.out.println("No existe esa posición");
+        
+        } catch(NumberFormatException nfe){
+            System.out.println("Eso no es un numero");
+        
+        } catch(Exception e){
+            System.out.println("Ha sucedido un error");
+        
+        } finally {
+            System.out.println("Hasta luego");
+        }
+        
+        
+
+        
     }
 }
